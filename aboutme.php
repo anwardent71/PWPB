7417aa4e3ae230d2b4b865c1b889ebd7c7746c89
Muhammad Anwar <52972399+anwardent71@users.noreply.github.com>
2020-04-08 13:57:26 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo "Nama : Muhammad Anwar<br>";
 echo "Kelas : XI Rpl 2<br>";
 echo "Sekolah : SMK Taruna Bhakti Depok<br>";
-echo "Alamat : Kp. Babakan Rawa Kalong Rt 02 Rw 08 No.14";
+echo "Nomor Handphone : 089611497541<br>";
+echo "Email : warrezi14@gmail.com";
 ?>
