f53de32619038e4e2afc62dae24d6a6e39e7ba9a
Muhammad Anwar <52972399+anwardent71@users.noreply.github.com>
2020-06-05 05:51:59 +0700

    Add files via upload

--- Ajax-no-Load/koneksi.php
+++ Ajax-no-Load/koneksi.php
@@ -1,1 +1,17 @@
+<?php
+// Connect Ke database
+    $DB_HOST = "localhost";
+    $DB_DATABASE = "db_starbhakno";
+    $DB_USERNAME = "root";
+    $DB_PASSWORD = "";
+    $DB_PORT = "3306";
 
+// Create Connection
+    $db1 = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
+
+// Check Connection
+    if($db1->connect_error) {
+        die('Connection Failed:' .$conn->connect_error);
+    }
+
+?>
